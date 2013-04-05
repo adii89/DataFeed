9a1647ec971d1351f414c960619db090050ba5ab
Adrian K <adi89xpl@yahoo.com>
2013-04-04 19:03:23 -0500

    project added with initial class started

---
+++ DataFeed/src/DataLayer/Instructor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataLayer;
+
+/**
+ *
+ * @author Adi
+ */
+public class Instructor {
+    private int _InstructorId;
+    private String _InstructorFirstName;
+    private String _InstructorLastName;
+    private char _InstrictorMiddleInit;
+    public
+}
