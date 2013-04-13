411aeacbcd57d0bef84f3cc5aa5a47dc7b6dfa4a
Erik <eamiller34@gmail.com>
2013-04-13 11:55:53 -0500

    new dept

---
+++ DataFeed/src/DataLayer/Dept.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataLayer;
+
+/**
+ *
+ * @author mille046
+ */
+public class Dept {
+    
+    private 
+    
+}
